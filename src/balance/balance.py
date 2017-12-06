df7a55413952b0c81e6822890e821a32f9568828
Andres <andres@data99.com.ar>
2017-12-06 01:03:32 -0300

    agrupacion final

--- src/balance/balance.py
+++ src/balance/balance.py
@@ -7,6 +7,40 @@ import requests
 import os
 import PyPDF2
 import csv
+import slugify
+
+
+class Balances:
+    ''' balances agrupados '''
+
+    balances_ingresos = []
+    balances_egresos = []
+    base_path = ''
+
+    def load(self, desde_anio=2017, hasta_anio=2017):
+
+
+        for anio in range(desde_anio, hasta_anio + 1):
+            BalanceIngresos(anio=anio, mes=1).clean_full_CSV()
+            BalanceEgresos(anio=anio, mes=1).clean_full_CSV()
+            for mes in range(1, 13):
+                b = BalanceIngresos(anio=anio, mes=mes)
+                b.base_path = self.base_path
+                if b.get_PDF():
+                    b.get_TXT()
+                    b.get_CSV()
+                    b.get_full_CSV()
+
+                    self.balances_ingresos.append(b)
+
+                b = BalanceEgresos(anio=anio, mes=mes)
+                b.base_path = self.base_path
+                if b.get_PDF():
+                    b.get_TXT()
+                    b.get_CSV()
+                    b.get_full_CSV()
+
+                    self.balances_egresos.append(b)
 
 
 class Balance:
@@ -87,6 +121,43 @@ class Balance:
         print('TXT a CSV {}-{}'.format(self.anio, self.mes))
         f.close()
 
+    def clean_full_CSV(self):
+        csv_file = self.save_CSV_path.format(self.base_path, self.anio, 'FULL')
+        if os.path.exists(csv_file):
+            os.remove(csv_file)
+
+    def get_full_CSV(self):
+        ''' pasar el TXT a un CSV acumulado '''
+        print('************* \n Pasar TXT a CSV {} {}'.format(self.anio, self.mes))
+        local_path = self.save_TXT_path.format(self.base_path, self.anio, self.mes)
+
+        f = open(local_path, 'r')
+        full_txt = f.read()
+        f.close()
+        lines = full_txt.split('\n')
+
+        csv_file = self.save_CSV_path.format(self.base_path, self.anio, 'FULL')
+        with open(csv_file, 'a', newline='') as csvfile:
+            fieldnames = self.fieldnames + ['anio', 'mes']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+
+            for line in lines:
+                # las líneas que me interesan tienen el códigop de la cuenta al inicio
+                cols = line.split('|')
+                if len(cols[0].split('.')) == 7:
+                    # print('Linea OK {}'.format(cols[0]))
+                    row = {'anio': self.anio, 'mes': self.mes}
+                    c = 0
+                    for field in self.fieldnames:
+                        row[field] = cols[c].strip()
+                        c += 1
+
+                    writer.writerow(row)
+
+        print('TXT a CSV {}-{}'.format(self.anio, self.mes))
+        f.close()
+
 
 class BalanceIngresos(Balance):
 
@@ -97,6 +168,9 @@ class BalanceIngresos(Balance):
     # explicacion de las columnas del PDF
     fieldnames = ['codigo', 'descripcion', 'Autorizado presupuesto', 'recaudación mes', 'acumulado','en menos', 'en más']
 
+    def __str__(self):
+        return 'Balance Ingresos {} {}'.format(self.anio, self.mes)
+
 class BalanceEgresos(Balance):
 
     save_PDF_path = '{}/PDFs/Egresos-{}-{}.pdf'
@@ -106,7 +180,8 @@ class BalanceEgresos(Balance):
     # explicacion de las columnas del PDF
     fieldnames = ['codigo', 'descripcion', 'Autorizado presupuesto', 'imputado mes', 'imputado acumulado','saldo a imputar', 'pagado mes', 'pagado acumulado', 'saldo a pagar']
 
-
+    def __str__(self):
+        return 'Balance Egresos {} {}'.format(self.anio, self.mes)
 
 
 
--- src/procesar-datos.py
+++ src/procesar-datos.py
@@ -1,24 +1,15 @@
 """
 procesar los balances de Mendiolaza publicados
 """
+import os
+from balance.balance import Balances
 
-from balance.balance import *
-from os.path import dirname, abspath
+b = Balances()
+prev_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+b.base_path = '{}/data'.format(prev_dir)
 
+# traer todos los meses
+b.load()
 
-for anio in range(2017, 2018):
-    for mes in range(1, 8):
-
-        b = BalanceIngresos(anio=anio, mes=mes)
-        prev_dir = dirname(dirname(abspath(__file__)))
-        b.base_path = '{}/data'.format(prev_dir)
-        if b.get_PDF():
-            b.get_TXT()
-            b.get_CSV()
-
-        b = BalanceEgresos(anio=anio, mes=mes)
-        prev_dir = dirname(dirname(abspath(__file__)))
-        b.base_path = '{}/data'.format(prev_dir)
-        if b.get_PDF():
-            b.get_TXT()
-            b.get_CSV()
+# ver que cargó
+print(b)
